65d6670106fcd878a8dbf367d7ea21868588aca2
Gerasimov Eugene <eugene@gromky.co>
2015-09-30 02:29:15 +0300

    speaker menu button isn't jumping anymore

--- wp-content/plugins/mindsummit-mobile-mockup-sidepanel/script.js
+++ wp-content/plugins/mindsummit-mobile-mockup-sidepanel/script.js
@@ -17,29 +17,6 @@ jQuery(function(){
 		$('<div class="speaker-menu-button"><div class="speaker-menu-button-text"></div></div>')
 			.appendTo($panel);
 	
-	//# buynow link
-	var $buynow_button = $('<div class="buynow-link">')
-		.appendTo('body');
-		
-	//# window resize handler
-	var _pb_width, _bb_height, _pb_shift, _bb_shift;
-	var _resize_handler = function(){
-			
-			if (window.getComputedStyle($panel.get(0)).display == 'none')
-				return;
-			
-			if (!_pb_width) _pb_width = $panel_button.outerWidth(true);
-			if (!_bb_height) _bb_height = $buynow_button.outerHeight(true);
-			if (!_pb_shift) _pb_shift = (_pb_width + _bb_height)/2;
-			if (!_bb_shift) _bb_shift = _pb_shift - _pb_width + _bb_height;
-			
-			var _w_middle = $(this).height()/2 - 50;
-			$panel_button.css('top', _w_middle - _pb_shift);
-			$buynow_button.css('top', _w_middle - _bb_shift);
-		};
-	$(window).resize(_resize_handler);
-	_resize_handler.call(window);
-	
 	//# open/close sliding panel
 	var _open = false;
 	var _p_width = $panel.outerWidth();
