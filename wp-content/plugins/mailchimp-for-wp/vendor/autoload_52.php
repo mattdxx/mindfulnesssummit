663fae12b01dbe8e1c3ec8a9acd68d76e53c2ae4
Daniel Espinoza <daniel@growdevelopment.com>
2015-09-28 23:34:59 -0500

    Updating MailChimp for WordPress Lite

--- wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
+++ wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit005fbf91077a87667f0bda159f1ec9b1::getLoader();
+return ComposerAutoloaderInit3d327b3703336a564033225b8d046767::getLoader();
