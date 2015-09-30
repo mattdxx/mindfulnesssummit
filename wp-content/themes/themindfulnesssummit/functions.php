af06fe801ad739b3d79b5c23fd055966fff7d141
Daniel Espinoza <daniel@growdevelopment.com>
2015-09-30 10:19:38 -0500

    Minimizing checkout page.

--- wp-content/themes/themindfulnesssummit/functions.php
+++ wp-content/themes/themindfulnesssummit/functions.php
@@ -20,3 +20,28 @@ function woo_redirect_to_checkout() {
 
 	return $checkout_url;
 }
+
+/**
+ * Remove the Coupon form from the Checkout page
+ */
+remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
+
+/**
+ * Remove fields from the checkout page
+ */
+add_filter( 'woocommerce_checkout_fields' , 'mindsummit_custom_override_checkout_fields' );
+function mindsummit_custom_override_checkout_fields( $fields ) {
+
+
+	unset($fields['billing']['billing_company']);
+	unset($fields['billing']['billing_address_1']);
+	unset($fields['billing']['billing_address_2']);
+	unset($fields['billing']['billing_city']);
+	unset($fields['billing']['billing_postcode']);
+	unset($fields['billing']['billing_country']);
+	unset($fields['billing']['billing_state']);
+	unset($fields['billing']['billing_phone']);
+	unset($fields['order']['order_comments']);
+
+	return $fields;
+}
