5d3bef55b989996821cba5a4283ed92e70458b48
Stratos Nikolaidis <snikolaidis@gmail.com>
2015-10-12 16:58:16 +0200

    + New shortcode: [gdlr_image_switcher]
    Shortcode for the image switcher
    examples:
    [gdlr_image_switcher image="http://cdn.themindfulnesssummit.com/wp-content/uploads/2015/10/Joeseph-Goldstein-preview3.jpg"]
    [gdlr_image_switcher image="http://cdn.themindfulnesssummit.com/wp-content/uploads/2015/10/Joeseph-Goldstein-preview3.jpg" extraheight="1"]
    [gdlr_image_switcher image="http://cdn.themindfulnesssummit.com/wp-content/uploads/2015/10/Joeseph-Goldstein-preview3.jpg" extraheight="1" count="9" session="rick-hanson"]

--- wp-content/plugins/optimize-wordpress/optimize-wordpress.php
+++ wp-content/plugins/optimize-wordpress/optimize-wordpress.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Optimize Wordpress
 Description: Optimize wordpress to speed up the whole process
-Version: 1.4.8
+Version: 1.4.9
 Author: Stratos Nikolaidis
 Author URI: https://gr.linkedin.com/in/stratosnikolaidis
 Plugin URI: https://gr.linkedin.com/in/stratosnikolaidis
@@ -183,3 +183,55 @@ function gdlr_with_imageload_scripts() {
     wp_enqueue_style('gdlr-with-image', plugin_dir_url(__FILE__).'assets/css/gdlr-with-image.min.css', array(), '1.1.7');
     wp_enqueue_script('gdlr-with-image', plugin_dir_url(__FILE__).'assets/js/gdlr-with-image.min.js', array(), '1.1.7', true);
 }
+
+
+/*
+Shortcode for the image switcher
+examples:
+[gdlr_image_switcher image="http://cdn.themindfulnesssummit.com/wp-content/uploads/2015/10/Joeseph-Goldstein-preview3.jpg"]
+[gdlr_image_switcher image="http://cdn.themindfulnesssummit.com/wp-content/uploads/2015/10/Joeseph-Goldstein-preview3.jpg" extraheight="1"]
+[gdlr_image_switcher image="http://cdn.themindfulnesssummit.com/wp-content/uploads/2015/10/Joeseph-Goldstein-preview3.jpg" extraheight="1" count="9" session="rick-hanson"]
+*/
+function gdlr_image_switcher_func( $atts ) {
+    $params = shortcode_atts( array(
+        'image' => '',
+        'count' => '1',
+        'session' => 'mark-williams',
+        'extraheight' => '0',
+    ), $atts );
+
+    $extraheight = $params['extraheight'] != '0';
+    $has_image = isset($params['image']) && ($params['image'] != '');
+
+    ob_start();
+?>
+<?php if ($has_image): ?>
+	<div class="gdlr-item gdlr-column-shortcode with-image <?php if ($extraheight): ?>extra-height<?php endif ?>">
+		<div class="gdlr-image-wrapper static">
+			<img src="<?php echo $params['image'] ?>" alt="" />
+		</div>
+<?php else: ?>
+	<div class="gdlr-item gdlr-column-shortcode">
+<?php endif ?>
+		<div class="gdlr-shortcode-wrapper">
+			<div class="gdlr-box-with-icon-ux gdlr-ux" style="opacity: 1; padding-top: 0px; margin-bottom: 0px;">
+				<div class="gdlr-item gdlr-box-with-icon-item pos-left type-normal" style="background-color: #303c41 !important; color: #fff !important; margin: 0; position: relative;">
+					<div class="box-with-icon-caption" style="font-size: 14px;">The free period to access this video and audio content has ended.
+						You can unlock it to stream and download 100% of the content for the summit immediately when you donate to mindfulness charities by upgrading to a full access pass.
+						<div class="gdlr-button-wrapper">
+							<a class="gdlr-button small" href="/?add_to_cart=4067" target="_self">Donate for Full Access Pass</a>
+						</div>
+						<p>View today’s free live session <a href="/live">here</a></p>
+						<p>Day <?php echo $params['count'] ?>’s Introduction to Mindfulness is available <a href="/sessions/<?php echo $params['session'] ?>/">here</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php
+	$contents = ob_get_contents();
+	ob_end_clean();
+
+    return $contents;
+}
+add_shortcode( 'gdlr_image_switcher', 'gdlr_image_switcher_func' );
