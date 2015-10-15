4e4456133c1f68d8839398ade3d8630659048f31
Stratos Nikolaidis <snikolaidis@gmail.com>
2015-10-15 13:25:25 +0200

    + gdlr-with-image: Load the assets only when we use the shortcode + Remove Segment_curl code (solution is through cookies) + Remove comments

--- wp-content/plugins/optimize-wordpress/optimize-wordpress.php
+++ wp-content/plugins/optimize-wordpress/optimize-wordpress.php
@@ -2,68 +2,11 @@
 /*
 Plugin Name: Optimize Wordpress
 Description: Optimize wordpress to speed up the whole process
-Version: 1.4.9
+Version: 1.4.10
 Author: Stratos Nikolaidis
 Author URI: https://gr.linkedin.com/in/stratosnikolaidis
 Plugin URI: https://gr.linkedin.com/in/stratosnikolaidis
 License: Toptal
-*/
-
-
-/*
-Prevent Contact Form 7 to reload the contact us forms data, in case the page is cached.
-*/
-
-/*
-
-Disabled for now, need to complete tests first
-
-remove_action( 'wp_enqueue_scripts', 'wpcf7_do_enqueue_scripts' );
-
-add_action( 'wp_enqueue_scripts', 'wpcf7_do_enqueue_scripts_with_no_cache' );
-function wpcf7_do_enqueue_scripts_with_no_cache() {
-	if ( wpcf7_load_js() ) {
-		wpcf7_enqueue_scripts_with_no_cache();
-	}
-
-	if ( wpcf7_load_css() ) {
-		wpcf7_enqueue_styles();
-	}
-}
-
-function wpcf7_enqueue_scripts_with_no_cache() {
-	// jquery.form.js originally bundled with WordPress is out of date and deprecated
-	// so we need to deregister it and re-register the latest one
-	wp_deregister_script( 'jquery-form' );
-	wp_register_script( 'jquery-form',
-		wpcf7_plugin_url( 'includes/js/jquery.form.min.js' ),
-		array( 'jquery' ), '3.51.0-2014.06.20', true );
-
-	$in_footer = true;
-
-	if ( 'header' === wpcf7_load_js() ) {
-		$in_footer = false;
-	}
-
-	wp_enqueue_script( 'contact-form-7',
-		wpcf7_plugin_url( 'includes/js/scripts.js' ),
-		array( 'jquery', 'jquery-form' ), WPCF7_VERSION, $in_footer );
-
-	$_wpcf7 = array(
-		'loaderUrl' => wpcf7_ajax_loader(),
-		'sending' => __( 'Sending ...', 'contact-form-7' ) );
-
-	// if ( defined( 'WP_CACHE' ) && WP_CACHE )
-	// 	$_wpcf7['cached'] = 1;
-
-	if ( wpcf7_support_html5_fallback() )
-		$_wpcf7['jqueryUi'] = 1;
-
-	wp_localize_script( 'contact-form-7', '_wpcf7', $_wpcf7 );
-
-	do_action( 'wpcf7_enqueue_scripts' );
-}
-
 */
 
 
@@ -86,6 +29,7 @@ function forgot_password_email($message, $key) {
   return $message;
 }
 
+
 /**
  * Disable plugins for specific pages in order to speed up the page load
  */
@@ -101,61 +45,8 @@ function disable_plugins_on_demand($plugins){
     return $plugins;
 }
 
-require_once(dirname(__FILE__) . "/Segment_curl.php");
-Segment_curl::init(
-	"wcHZ0hb6Xk6YaoouR6EKNqWYlxwXn6Hu",
-	array(
-		"consumer" => "fork_curl",
-		"debug" => true,
-		"max_queue_size" => 5,
-		"batch_size" => 5,
-	)
-);
 
-//add_action( 'user_register', 'analytics_on_user_registration', 10, 1 );
-function analytics_on_user_registration( $user_id ) {
-    // if ( isset( $_POST['first_name'] ) )
-    //     update_user_meta($user_id, 'first_name', $_POST['first_name']);
-    if ( $user = get_userdata( $user_id ) ) {
-    	Segment_curl::identify(
-		    array(
-		       'userId' => $user->ID,
-		       'traits'  => array(
-					'username'  => $user->user_login,
-					'email'     => $user->user_email,
-					'firstName' => $user->user_firstname,
-					'lastName'  => $user->user_lastname,
-		       	)
-		    )
-		);
-    	Segment_curl::track(
-		    array(
-		       	'userId' => $user->ID,
-				'event' => 'User registered',
-		    )
-		);
-    }
-}
-
-add_action( 'wc_memberships_grant_membership_access_from_purchase', 'tms_track_full_access_pass', 10, 2 );
-function tms_track_full_access_pass( $membership_plan, $args ) {
-
-	// If the tracking library isn't active, stop here
-	if ( !class_exists( 'Segment_Cookie' ) ) {
-		return;
-	}
-
-	// If this isn't the full access pass membership plan, stop
-	if ( !$membership_plan->id == 3737 ) {
-		return;
-	}
-
-	$user_id = $args['user_id'];
-	Segment_Cookie::set_cookie( 'Full Access Pass', json_encode( $user_id ) );
-}
-
-
-/* v.1.1.7
+/* v.1.1.8
 How to use the following scripts. Right now, the shortcode [wcm_restrict plans="free-members"] is like this:
 In case the image is higher than the textbox, we can add the extra class called "extra-height" and also, we need to
 wrap the button in a <div class="gdlr-button-wrapper"> wrapper.
@@ -178,12 +69,23 @@ page will fade out the image and replace it with the the gdlr-shortcode section 
 
 Yes, it's responsive also. :)
 */
-add_action('wp_enqueue_scripts', 'gdlr_with_imageload_scripts', 99);
-function gdlr_with_imageload_scripts() {
-    wp_enqueue_style('gdlr-with-image', plugin_dir_url(__FILE__).'assets/css/gdlr-with-image.min.css', array(), '1.1.7');
-    wp_enqueue_script('gdlr-with-image', plugin_dir_url(__FILE__).'assets/js/gdlr-with-image.min.js', array(), '1.1.7', true);
+$add_gdlr_with_image_scripts = false;
+add_action('init', 'register_gdlr_with_image_script_script');
+add_action('wp_footer', 'print_gdlr_with_image_script_script');
+function register_gdlr_with_image_script_script() {
+    wp_register_script('gdlr-with-image', plugin_dir_url(__FILE__).'assets/js/gdlr-with-image.min.js', array(), '1.1.8', true);
+    wp_register_style('gdlr-with-image', plugin_dir_url(__FILE__).'assets/css/gdlr-with-image.min.css', array(), '1.1.8');
 }
 
+function print_gdlr_with_image_script_script() {
+	global $add_gdlr_with_image_scripts;
+
+	if (!$add_gdlr_with_image_scripts)
+		return;
+
+	wp_print_styles('gdlr-with-image');
+	wp_print_scripts('gdlr-with-image');
+}
 
 /*
 Shortcode for the image switcher
@@ -193,6 +95,9 @@ examples:
 [gdlr_image_switcher image="http://cdn.themindfulnesssummit.com/wp-content/uploads/2015/10/Joeseph-Goldstein-preview3.jpg" extraheight="1" count="9" session="rick-hanson"]
 */
 function gdlr_image_switcher_func( $atts ) {
+	global $add_gdlr_with_image_scripts;
+	$add_gdlr_with_image_scripts = true;
+
     $params = shortcode_atts( array(
         'image' => '',
         'count' => '1',
