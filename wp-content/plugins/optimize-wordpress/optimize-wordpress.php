5b6f142d09cd091dc06c2a08bf037ba6a08ae969
Stratos Nikolaidis <snikolaidis@gmail.com>
2015-10-28 15:43:53 +0100

    + Testing mailchimp registration

--- wp-content/plugins/optimize-wordpress/optimize-wordpress.php
+++ wp-content/plugins/optimize-wordpress/optimize-wordpress.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Optimize Wordpress
 Description: Optimize wordpress to speed up the whole process
-Version: 1.5.15
+Version: 1.5.16
 Author: Stratos Nikolaidis
 Author URI: https://gr.linkedin.com/in/stratosnikolaidis
 Plugin URI: https://gr.linkedin.com/in/stratosnikolaidis
